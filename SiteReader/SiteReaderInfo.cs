8ba63b1904560689bb7a881b0d12f2f77cbe33f8
aarcThom <thomas@aarc.io>
2023-12-15 20:57:19 -0800

    Add project files.

---
+++ SiteReader/SiteReaderInfo.cs
@@ -0,0 +1,26 @@
+﻿using Grasshopper;
+using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace SiteReader
+{
+    public class SiteReaderInfo : GH_AssemblyInfo
+    {
+        public override string Name => "SiteReader";
+
+        //Return a 24x24 pixel bitmap to represent this GHA library.
+        public override Bitmap Icon => null;
+
+        //Return a short string describing the purpose of this GHA library.
+        public override string Description => "";
+
+        public override Guid Id => new Guid("3ec7d2c2-de40-4f03-b37d-58eef67c5a5b");
+
+        //Return a string identifying you or your company.
+        public override string AuthorName => "";
+
+        //Return a string representing your preferred contact details.
+        public override string AuthorContact => "";
+    }
+}
