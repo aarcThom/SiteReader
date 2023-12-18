9690555ef64bbd6dafeacbb13cb4be7b58d86cd6
aarcThom <thomas@aarc.io>
2023-12-18 00:16:53 -0800

    Implement UI base class and UI component interface

---
+++ SiteReader/UI/Components/Button.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Grasshopper.GUI.Canvas;
+using Grasshopper.Kernel;
+
+namespace SiteReader.UI.Components
+{
+    public class Button : IUi
+    {
+        //FIELDS ======================================================================================================
+        private string _text;
+
+        private RectangleF _bounds;
+
+        //PROPERTIES ==================================================================================================
+        public float Width { get; set; }
+        public float Height { get; set; }
+        public float Bottom { get; set; }
+        public float SideSpace { get; set; }
+        public GH_Component Owner { get; set; }
+
+        //CONSTRUCTORS ================================================================================================
+
+
+        public Button(string text, float height)
+        {
+            _text = text;
+            Height = height;
+        }
+
+        public void Layout(RectangleF ownerRectangleF, float yPos)
+        {
+            float buttonWidth = Width == 0 ? ownerRectangleF.Width : Width;
+
+            if (yPos == 0)
+            {
+                throw new Exception("yPos must be defined!");
+            }
+
+
+            _bounds = new RectangleF(ownerRectangleF.Left, yPos, buttonWidth, Height);
+
+            Bottom = _bounds.Bottom;
+
+            _bounds.Inflate(-SideSpace, 0);
+        }
+
+        public void Render(Graphics g)
+        {
+            GH_Capsule button = GH_Capsule.CreateTextCapsule(_bounds, _bounds, GH_Palette.Black, _text);
+            button.Render(g, false, Owner.Locked, false);
+        }
+
+    }
+}
---
+++ SiteReader/UI/UIBase.cs
@@ -0,0 +1,73 @@
+﻿using Grasshopper.GUI.Canvas;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using SiteReader.UI.Components;
+
+namespace SiteReader.UI
+{
+    public abstract class UIBase : GH_ComponentAttributes
+    {   
+        //FIELDS ======================================================================================================
+        protected RectangleF OwnerRectangle;
+        protected List<IUi> ComponentList;
+
+        private readonly GH_Component _parent;
+
+        // Values to change if you want to play with layout
+        private const int SideSpace = 2;
+        private const int VertSpace = 10;
+
+        //PROPERTIES ==================================================================================================
+
+        //CONSTRUCTORS ================================================================================================
+        protected UIBase(GH_Component owner) : base(owner)
+        {
+            _parent = owner;
+        }
+
+        //LAYOUT ======================================================================================================
+        protected override void Layout()
+        {
+            base.Layout(); //handles the basic layout, computes the bounds, etc.
+            OwnerRectangle = GH_Convert.ToRectangle(Bounds); //getting component base bounds
+
+            float yPos = OwnerRectangle.Bottom + VertSpace;
+            int extraHeight = VertSpace;
+            if ( ComponentList != null && ComponentList.Count > 0)
+            {
+                foreach (var uiComp in ComponentList)
+                {
+                    uiComp.Owner = _parent;
+                    uiComp.SideSpace = SideSpace;
+
+                    extraHeight += (int)uiComp.Height + VertSpace;
+                    uiComp.Layout(OwnerRectangle, yPos);
+                    yPos += uiComp.Height + VertSpace;
+                }
+            }
+
+            OwnerRectangle.Height += extraHeight;
+            Bounds = OwnerRectangle;
+
+        }
+
+        //RENDER ======================================================================================================
+        protected override void Render(GH_Canvas canvas, Graphics graphics, GH_CanvasChannel channel)
+        {
+            base.Render(canvas, graphics, channel); // handle the wires, draw nickname, name, etc.
+            if (ComponentList != null && ComponentList.Count > 0)
+            {
+                foreach (var uiComp in ComponentList)
+                {
+                    uiComp.Render(graphics);
+                }
+            }
+        }
+    }
+}
